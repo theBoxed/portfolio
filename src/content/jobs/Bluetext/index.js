ce1f6231e9c6199a1bcc0ddf4fde1aa56528b031
Box <40639989+theBoxed@users.noreply.github.com>
2020-02-24 12:17:21 -0700

    added say hwdy

--- src/content/jobs/Bluetext/index.js
+++ src/content/jobs/Bluetext/index.js
@@ -1,68 +1,79 @@
 const jobData = [
   {
-      company: 'Rehab.com',
-      date: '2019-4-16',
-      title: 'Strategy and Operations',
-      location: 'Washington D.C.',
-      range: 'April 2019 - February 2020',
-      url: 'https://rehab.com/',
-      duties: [
-        'Help identify strategic needs, leading all aspects of planning with sound business recommendations, and generating alignment with key stakeholders.',
-        'Champion internal systems for higher productivity working.'
-      ]
+    company: 'Say Hwdy',
+    date: '2019-4-16',
+    title: 'Founder',
+    location: 'Denver, CO',
+    range: 'April 2019 - Present',
+    url: 'https://bradycfox.com',
+    duties: [
+      'High-level strategy and Natural born problem solvers, Say Hwdy is the digital agency that will help you create smart, results-driven experiences that engage people, transform brands, grow businesses, and drive stronger connections.'
+    ]
   },
-{
-      company: 'Bluetext',
-      date: '2015-3-16',
-      title: 'Director of Marketing Programs',
-      location: 'Washington D.C.',
-      range: 'March 2015 - May 2019',
-      url: 'https://www.bluetext.com/',
-      duties: [
-        'Worked closerly with team to grow the agency’s marketing programs from the ground up resulting in additional revenue streams.',
-        'Responsible for the development and management of all client brand awareness & demand generation marketing programs, including strategic planning, KPI goals, budget management, execution and ROI tracking. ',
-        'Manage a small team of on-site as well as off-site marketing professionals focused on executing campaigns including email, display advertising, paid social, partnership agreements, content marketing, and other various digital marketing channels.'
-      ]
+  {
+    company: 'Rehab.com',
+    date: '2019-4-16',
+    title: 'Strategy and Operations',
+    location: 'Washington D.C.',
+    range: 'April 2019 - February 2020',
+    url: 'https://rehab.com/',
+    duties: [
+      'Help identify strategic needs, leading all aspects of planning with sound business recommendations, and generating alignment with key stakeholders.',
+      'Champion internal systems for higher productivity working.'
+    ]
   },
-    {
-      company: 'New North',
-      date: '2014-7-14',
-      title: 'Project Manager',
-      location: 'Washington D.C.',
-      range: 'July 2014 - March 2015',
-      url: 'https://www.newnorth.com/',
-      duties: [
-        'Focused on building and delivering client services to identify key weaknesses and improve their online marketing performances.',
-        'Identified internal businesses opportunities, and influenced leadership to expand our business offerings to increase MRR and increase client retention.',
-      ]
-    },
-    {
-      company: 'Better World',
-      date: '2013-7-14',
-      title: 'Co-Founder',
-      location: 'NYC',
-      range: 'July 2013 - June 2014',
-      url: 'https://betterworld.org/',
-      duties: [
-        'Designed and led the founding team in the development and implementation of our suite of products that have shifted the way non-profits approach giving.',
-        'Offered two different board positions due to my ability to lead customer through their journey towards being better, more donor driven organizations.',
-        'Built the systems responsible for managing the cumulative customer experience across multiple touch point and multiple channels.'
-      ]
-    },
-    {
-      company: 'UVA Swimming',
-      date: '2013-3-31',
-      title: 'Team Member & Captain',
-      location: 'Charlottesville, NYC',
-      range: 'August 2009 - March 2013',
-      url: 'https://virginiasports.com/index.aspx?path=swimming',
-      duties: [
-        'Four time ACC Champion & Captain of the 2012-2013 ACC Championship Swim Team.',
-        'Recipient of Ramirez Family Leadership Scholarship',
-        'Two-Time Olympic Trial Participant',
-        'Former National Record Holder in the 200 yard Backstroke'
-      ]
-    }
+  {
+    company: 'Bluetext',
+    date: '2015-3-16',
+    title: 'Director of Marketing Programs',
+    location: 'Washington D.C.',
+    range: 'March 2015 - May 2019',
+    url: 'https://www.bluetext.com/',
+    duties: [
+      'Worked closerly with team to grow the agency’s marketing programs from the ground up resulting in additional revenue streams.',
+      'Responsible for the development and management of all client brand awareness & demand generation marketing programs, including strategic planning, KPI goals, budget management, execution and ROI tracking. ',
+      'Manage a small team of on-site as well as off-site marketing professionals focused on executing campaigns including email, display advertising, paid social, partnership agreements, content marketing, and other various digital marketing channels.'
+    ]
+  },
+  {
+    company: 'New North',
+    date: '2014-7-14',
+    title: 'Project Manager',
+    location: 'Washington D.C.',
+    range: 'July 2014 - March 2015',
+    url: 'https://www.newnorth.com/',
+    duties: [
+      'Focused on building and delivering client services to identify key weaknesses and improve their online marketing performances.',
+      'Identified internal businesses opportunities, and influenced leadership to expand our business offerings to increase MRR and increase client retention.'
+    ]
+  },
+  {
+    company: 'Better World',
+    date: '2013-7-14',
+    title: 'Co-Founder',
+    location: 'NYC',
+    range: 'July 2013 - June 2014',
+    url: 'https://betterworld.org/',
+    duties: [
+      'Designed and led the founding team in the development and implementation of our suite of products that have shifted the way non-profits approach giving.',
+      'Offered two different board positions due to my ability to lead customer through their journey towards being better, more donor driven organizations.',
+      'Built the systems responsible for managing the cumulative customer experience across multiple touch point and multiple channels.'
+    ]
+  },
+  {
+    company: 'UVA Swimming',
+    date: '2013-3-31',
+    title: 'Team Member & Captain',
+    location: 'Charlottesville, NYC',
+    range: 'August 2009 - March 2013',
+    url: 'https://virginiasports.com/index.aspx?path=swimming',
+    duties: [
+      'Four time ACC Champion & Captain of the 2012-2013 ACC Championship Swim Team.',
+      'Recipient of Ramirez Family Leadership Scholarship',
+      'Two-Time Olympic Trial Participant',
+      'Former National Record Holder in the 200 yard Backstroke'
+    ]
+  }
 ];
 
 export { jobData };
